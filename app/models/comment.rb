c7517a1b01a07c4d99c7d39a626603bfcfcd51ea
Ian Hawkins <itsianhawk@gmail.com>
2010-10-17 17:29:39 -0400

    Added a captcha for commenting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  
+  validate :check_captcha
+  
+  attr_accessor :captcha
+  
+  def check_captcha
+	if @captcha.downcase.strip != "black"
+		errors.add_to_base "Captcha text was incorrect. Please try again."
+		return false
+	end
+	return true
+  end
 end
