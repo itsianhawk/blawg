2a43dc2c6a5d758ea0bdfa94c23146b5c84fb705
unknown <Ian@.(none)>
2010-10-24 15:43:34 -0400

    captcha removed from edit comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,15 +2,15 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   
-  validate :check_captcha
+  before_create :check_captcha
   
   attr_accessor :captcha
   
   def check_captcha
-	if @captcha.downcase.strip != "black"
-		errors.add_to_base "Captcha text was incorrect. Please try again."
-		return false
-	end
-	return true
+	  if @captcha.downcase.strip != "black"
+		  errors.add_to_base "Captcha text was incorrect. Please try again."
+		  return false
+	  end
+	  return true
   end
 end
